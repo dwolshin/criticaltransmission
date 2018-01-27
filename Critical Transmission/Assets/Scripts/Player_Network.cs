6861354e08035d65c13f2e5a670c4dbec1311536
dwolshin <dwolshin@hotmail.com>
2018-01-27 09:56:19 -0500

    Updated Networking scripts - working basic

---
+++ Critical Transmission/Assets/Scripts/Player_Network.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Player_Network : NetworkBehaviour {
+	public GameObject FirstPersonCharacter;
+	public GameObject[] CharactersModel;
+
+	public override void OnStartLocalPlayer ()
+	{
+
+
+		GetComponent<FirstPersonController> ().enabled = true;
+		FirstPersonCharacter.SetActive (true);
+
+		foreach (GameObject go in CharactersModel) {
+			go.SetActive (false);
+		}
+	}
+}

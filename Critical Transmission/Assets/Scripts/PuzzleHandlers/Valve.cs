ace98a8f48b4961d457860a273c3c4387f1d77fc
Greg <gregoryswestland@gmail.com>
2018-01-27 14:15:15 -0500

    Puzzle greybox

--- Critical Transmission/Assets/Scripts/PuzzleHandlers/Valve.cs
+++ Critical Transmission/Assets/Scripts/PuzzleHandlers/Valve.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 //Valve's pretty okay.
 public class Valve : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public const float maxValue = 1;
+	public const float minValue = 0;
+	public float value = 0;
+	public float valuePerSecond;
+
+	void Update() {
+		TurnValve (false);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void TurnValve(bool isClockwise) {
+		if (!isClockwise) {
+			value -= valuePerSecond * Time.deltaTime;
+			if (value < minValue)
+				value = minValue;
+			transform.Rotate (new Vector3 (0, 0, -5f), Space.Self);
+		}
+		if (isClockwise) {
+			value += valuePerSecond * Time.deltaTime;
+			if (value > maxValue)
+				value = maxValue;
+			transform.Rotate (new Vector3 (0, 0, 5f), Space.Self);
+
+		}
 	}
 }

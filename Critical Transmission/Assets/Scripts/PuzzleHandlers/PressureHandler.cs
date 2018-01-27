3e43916468ad7990dbc233e48adfef2612f397f4
Greg <gregoryswestland@gmail.com>
2018-01-27 13:36:23 -0500

    Initial Puzzle code

---
+++ Critical Transmission/Assets/Scripts/PuzzleHandlers/PressureHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PressureHandler : NetworkBehaviour {
+
+	public Valve[] valves;
+	public float[] valveTargets;
+	public float forgivenessAmount;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Critical Transmission/Assets/Scripts/PuzzleHandlers/Valve.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Valve's pretty okay.
+public class Valve : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

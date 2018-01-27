f9061c8cb119cc3e2ba29e0c82dcd6f74d68001f
Greg <gregoryswestland@gmail.com>
2018-01-27 08:24:38 -0500

    Networked timer & initial interaction block code

---
+++ Critical Transmission/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerBehaviour : NetworkBehaviour {
+
+	public static PlayerBehaviour singleton;
+
+	private const float maxInteractDistance = 1f;
+
+	private Transform currentInteractedObject;
+
+	private GameObject fpsCamera;
+
+	public override void OnStartLocalPlayer()
+	{
+		singleton = this;
+		transform.Find ("UI").gameObject.SetActive (true);
+		fpsCamera = transform.Find ("FirstPersonCharacter").gameObject;
+		fpsCamera.SetActive(true);
+		transform.Find("Cube").GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+
+	public void Update() {
+
+		if (Input.GetButtonDown("Fire1")) {
+			RaycastHit hit;
+			if (Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, maxInteractDistance)) {
+
+			}
+		}
+
+		if (Input.GetButton("Fire1")) {
+
+		}
+	}
+}
---
+++ Critical Transmission/Assets/Scripts/SinkTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SinkTimer : NetworkBehaviour {
+
+	//Singleton
+	public static SinkTimer s;
+
+	//Bools
+	public bool timerRunning = true;
+	public bool timerAccelerated = false;
+
+	//Timer
+	public const float accelerationAmount = 0.5f;
+	public const float maxTime = 120f;
+	[SyncVar]
+	private float remainingTime;
+
+	void Start() {
+		s = this;
+		if (isServer) remainingTime = maxTime;
+	}
+
+	void Update() {
+		if (!isServer)
+			return;
+		if (timerRunning) {
+			remainingTime -= Time.deltaTime;
+			if (timerAccelerated) {
+				remainingTime -= Time.deltaTime * accelerationAmount;
+			}
+		}
+	}
+
+	public float GetRemainingTime() {
+		return remainingTime;
+	}
+}
